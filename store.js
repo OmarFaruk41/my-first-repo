4dacc9a3aff8d81834fe32cab65ded59b8e8c8db
OmarFaruk41 <omarfaruk.nsn.0161@gmail.com>
2025-07-15 20:38:31 +0600

    3rd commit

--- store.js
+++ store.js
@@ -6,6 +6,9 @@ function handleSearch() {
 
 const loadPhone = async (searchText) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
-    const data = await res.json();
-    console.log(data);
+    const searverData= await res.json();
+    displayPhones(searverData.data);
 };
+const displayPhones = (data) => {
+console.log("Inside displayPhones function:", data);
+}

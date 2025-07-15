3c12c759bcc52e32d008fdd806a7580e1635300d
OmarFaruk41 <omarfaruk.nsn.0161@gmail.com>
2025-07-15 22:52:13 +0600

    5th commit

--- store.js
+++ store.js
@@ -6,12 +6,36 @@ function handleSearch() {
 
 const loadPhone = async (searchText) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
-    const searverData= await res.json();
-    displayPhones(searverData.data);
+    const serverData = await res.json();
+    displayPhones(serverData.data);
 };
+
 const displayPhones = (data) => {
     console.log(data);
-const cardsContainer = document.getElementById('card-section');
-data.forEach(phone => {
-    
-}
+    const cardsContainer = document.getElementById('card-section');
+
+    //  clear previous results
+    cardsContainer.innerHTML = '';
+
+    data.forEach(singlePhone => {
+        const productCard = document.createElement('div');
+        productCard.classList.add('card');
+        productCard.innerHTML = `
+            <div class="card-image">
+                <img src="${singlePhone.image}" alt="${singlePhone.phone_name}">
+            </div>
+            <h3 class="card-title">${singlePhone.phone_name}</h3>
+            <p class="card-description">
+                There are many variations of passages available, but the majority have suffered.
+            </p>
+            <div class="card-price">
+                <span>$</span>
+                <span id="card-item-price">999</span>
+            </div>
+            <div class="card-button">
+                <button class="btn">Show Details</button>
+            </div>
+        `;
+        cardsContainer.appendChild(productCard);
+    });
+};

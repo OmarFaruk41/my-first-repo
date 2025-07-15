1c5491249ed45a27128e93d30a0e33c5d2eac629
OmarFaruk41 <omarfaruk.nsn.0161@gmail.com>
2025-07-15 18:17:52 +0600

    first commit

---
+++ store.js
@@ -0,0 +1,11 @@
+function handleSearch() {
+    const searchInputElement = document.getElementById('search-input-field');
+    const searchInputValue = searchInputElement.value;
+    loadPhone(searchInputValue);
+}
+
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+    const data = await res.json();
+    console.log(data);
+};
